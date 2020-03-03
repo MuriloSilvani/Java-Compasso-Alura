dde58116bb4bfd334ffbfff0a5d18a68ce3a5f5c
Murilo <murilo.silvani@gmail.com>
2020-03-03 11:58:10 -0300

    spring boot

---
+++ SpringBoot/src/main/java/br/com/murilo/listavip/repository/ConvidadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.murilo.listavip.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.murilo.listavip.model.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long> {
+
+}
